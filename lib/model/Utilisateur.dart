a9d9daaa14c64b52b8494b9372f86076250397ba
djinodissingar <dissingardjino@icloud.com>
2022-03-14 15:13:56 +0100

    Ajout de la classe Utilisateur

---
+++ lib/model/Utilisateur.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Utilisateur{
+  //Attributs
+  String id="";
+  String prenom="";
+  String? nom;
+  String mail="";
+  DateTime? birthday;
+  String? avatar;
+
+
+
+  //Constructeur
+Utilisateur(DocumentSnapshot snapshot){
+  id = snapshot.id;
+  Map<String,dynamic> map = snapshot.data() as Map<String,dynamic>;
+  prenom = map["PRENOM"];
+  nom = map["NOM"];
+  mail = map ["MAIL"];
+  birthday = map["BIRTHDAY"];
+  avatar = map["AVATAR"];
+}
+
+
+  //Methode
+}
